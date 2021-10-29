04d88e94624145984c6a06e09f87dc39ddb71487
haksa <haksa@LAPTOP-KJK6C5MT.mshome.net>
2021-10-29 10:10:47 +0900

    this is newlecture's YouTube lecture.

---
+++ src/main/java/com/pongdong/web/controller/indexController.java
@@ -0,0 +1,22 @@
+package com.pongdong.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class indexController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		ModelAndView mv = new ModelAndView("index");
+		mv.addObject("data","Sobong is BaBo~!!");
+		//mv.setViewName("/WEB-INF/view/index.jsp");
+
+		
+		return mv;
+	}
+	
+}
